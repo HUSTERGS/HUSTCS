0db16cc16a7b4cd94f7ee861cab587ed51ec4d94
HUSTERGS <1148690954@qq.com>
2019-02-28 21:47:18 +0800

    fix bugs

--- 数据结构课设/CourseDesign2.cpp
+++ 数据结构课设/CourseDesign2.cpp
@@ -1,7 +1,16 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <time.h>
+
+
 #define DISCARD_BUFFER_SIZE 100
+#define TRUE 1
+#define FALSE 0
+
+
+typedef int BOOL;
+
 
 struct intListNode {
 	int value;
@@ -16,18 +25,18 @@ struct intListHead
 
 struct Var
 {
-	int index; // 变元序号，没有正负
+	//int index; // 变元序号，没有正负
 	int positiveCount; // 正的数量
 	int negativeCount; // 负的数量
-	intListHead positive;
-	intListHead negative;
-	bool handled; // 表示该变元是否已经被赋值了，用于FindMax时排除变量
+	intListHead * positive;
+	intListHead * negative;
+	BOOL handled; // 表示该变元是否已经被赋值了，用于FindMax时排除变量
 };
 
 struct Clause
 {
 	int length;
-	intListHead variables; // 保存着子句中所有变元的位置
+	intListHead * variables; // 保存着子句中所有变元的位置
 	int hidden;  // 指示是否被删除，以及被删除的原因
 };
 
@@ -41,7 +50,7 @@ struct CNF {
 struct DecisionTreeNode
 {
 	int assumption;
-	bool valid;
+	BOOL valid;
 	DecisionTreeNode * next;
 	DecisionTreeNode * back;
 };
@@ -52,8 +61,10 @@ struct DecisionTreeHead
 };
 
 
+
+
 void addElem(int target, intListHead * head) {
-	intListNode * newNode = (intListNode *)calloc(1, sizeof(intListNode));
+	intListNode * newNode = (intListNode *)malloc(sizeof(intListNode));
 
 	assert(newNode != nullptr);
 	// old
@@ -61,24 +72,11 @@ void addElem(int target, intListHead * head) {
 	newNode->next = head->first;
 	head->first = newNode;
 	++head->length;
-	
-	// new
-	/*newNode->value = target;
-	if (head->first)
-	{
-		newNode->next = head->first;
-		head->first = newNode;
-	}
-	else {
-		head->first = newNode;
-		newNode->next = nullptr;
-	}
-	head->length++;*/
 }
 
 void deleteElem(int target, intListHead * head) {
 	// 其实实际上并没有删除，只是将其放到链表的最后一个，并将length减去1
-	intListNode * temp = head->first;
+	/*intListNode * temp = head->first;
 	intListNode * pre = temp;
 	if (head->first->value == target) {
 		while (temp->next)
@@ -103,26 +101,26 @@ void deleteElem(int target, intListHead * head) {
 		pre->next = pre->next->next;
 		temp->next->next = nullptr;
 		--head->length;
-	}
-	//--head->length;
+	}*/
+	--head->length;
 }
 
 void recoverElem(int target, intListHead * head) {
 	// 如果是当前assumption导致的删除的话，必然是最后一个元素,如果最开始是单子句的话，也不可能需要回溯
-	intListNode * tempNode = head->first;
+	/*intListNode * tempNode = head->first;
 	while (tempNode->next->value != target)
 		tempNode = tempNode->next;
 	tempNode->next->next = head->first;
 	head->first = tempNode->next;
-	tempNode->next = nullptr;
+	tempNode->next = nullptr;*/
 	++head->length;
 }
 
-DecisionTreeNode * addAssumption(int assumption, bool valid, DecisionTreeHead * head) {
+DecisionTreeNode * addAssumption(int assumption, BOOL valid, DecisionTreeHead * head) {
 	DecisionTreeNode * tempNode = head->firstNode;
 	if (tempNode == nullptr) {
 		// 此时还没有任何assumption
-		head->firstNode = (DecisionTreeNode *)calloc(1, sizeof(DecisionTreeNode));
+		head->firstNode = (DecisionTreeNode *)malloc(sizeof(DecisionTreeNode));
 
 		assert(head->firstNode != nullptr);
 
@@ -135,7 +133,7 @@ DecisionTreeNode * addAssumption(int assumption, bool valid, DecisionTreeHead * 
 	while (tempNode->next)
 		tempNode = tempNode->next;
 	// 找到最后一个节点
-	tempNode->next = (DecisionTreeNode *)calloc(1, sizeof(DecisionTreeNode));
+	tempNode->next = (DecisionTreeNode *)malloc(sizeof(DecisionTreeNode));
 
 	assert(tempNode->next != nullptr);
 
@@ -147,21 +145,33 @@ DecisionTreeNode * addAssumption(int assumption, bool valid, DecisionTreeHead * 
 }
 
 void InitIntListHead(CNF * cnf) {
-	cnf->clauses = (Clause *)calloc((cnf->clauseNum + 1), sizeof(Clause));
-	cnf->variables = (Var *)calloc((cnf->varNum + 1), sizeof(Var));
+	cnf->clauses = (Clause *)malloc((cnf->clauseNum + 1) * sizeof(Clause));
+	cnf->variables = (Var *)malloc((cnf->varNum + 1) * sizeof(Var));
 
 	assert(cnf->clauses != nullptr && cnf->variables != nullptr);
 
 	for (int i = cnf->clauseNum; i > 0; --i) {
-		cnf->clauses[i].variables.length = 0;
+
+		cnf->clauses[i].length = 0;
+		cnf->clauses[i].variables = (intListHead *)malloc(sizeof(intListHead));
+		assert(cnf->clauses[i].variables != nullptr);
+
+		cnf->clauses[i].variables->length = 0;
 		cnf->clauses[i].hidden = 0;
-		cnf->clauses[i].variables.first = nullptr;
+		cnf->clauses[i].variables->first = nullptr;
 	}
 	for (int i = cnf->varNum; i > 0; --i) {
-		cnf->variables[i].negative.length = 0;
-		cnf->variables[i].positive.length = 0;
-		cnf->variables[i].negative.first = nullptr;
-		cnf->variables[i].positive.first = nullptr;
+
+		cnf->variables[i].negative = (intListHead *)malloc(sizeof(intListHead));
+		cnf->variables[i].positive = (intListHead *)malloc(sizeof(intListHead));
+		assert(cnf->variables[i].negative != nullptr);
+		assert(cnf->variables[i].positive != nullptr);
+
+		cnf->variables[i].negative->length = 0;
+		cnf->variables[i].positive->length = 0;
+		cnf->variables[i].negative->first = nullptr;
+		cnf->variables[i].positive->first = nullptr;
+		cnf->variables[i].handled = FALSE;
 	}
 }
 
@@ -174,14 +184,14 @@ int LoadFile(const char * filename, CNF * cnf) {
 		char firstChar = fgetc(fp);
 		while ('c' == firstChar) {
 			// 如果是 c 说明是注释，直接跳到下一行
-			char * discardBuffer = (char *)calloc(DISCARD_BUFFER_SIZE, sizeof(char));
+			char * discardBuffer = (char *)malloc(DISCARD_BUFFER_SIZE * sizeof(char));
 			fgets(discardBuffer, DISCARD_BUFFER_SIZE, fp);
 			firstChar = fgetc(fp);
 			free(discardBuffer);
 			discardBuffer = nullptr;
 		}
 		if ('p' == firstChar) {
-			char * discardBuffer = (char *)calloc(DISCARD_BUFFER_SIZE, sizeof(char));
+			char * discardBuffer = (char *)malloc(DISCARD_BUFFER_SIZE * sizeof(char));
 			fscanf(fp, "%s", discardBuffer); // 跳过cnf 字符串
 			fscanf(fp, "%d", &cnf->varNum); // 读取变元数
 			fscanf(fp, "%d", &cnf->clauseNum); // 读取子句数
@@ -204,14 +214,12 @@ int LoadFile(const char * filename, CNF * cnf) {
 			while (temp)
 			{
 				if (temp > 0) {
-					cnf->variables[temp].handled = false;
-					addElem(i, &cnf->variables[temp].positive);
-					addElem(temp, &cnf->clauses[i].variables);
+					addElem(i, cnf->variables[temp].positive);
+					addElem(temp, cnf->clauses[i].variables);
 				}
 				else {
-					cnf->variables[temp].handled = false;
-					addElem(i, &cnf->variables[-temp].negative);
-					addElem(temp, &cnf->clauses[i].variables);
+					addElem(i, cnf->variables[-temp].negative);
+					addElem(temp, cnf->clauses[i].variables);
 				}
 				++clauseLength;
 				fscanf(fp, "%d", &temp);
@@ -227,7 +235,7 @@ int LoadFile(const char * filename, CNF * cnf) {
 void DisplayData(CNF * cnf) {
 	int clauseNum = cnf->clauseNum;
 	for (int i = 1; i <= clauseNum; ++i) {
-		intListNode * temp = cnf->clauses[i].variables.first;
+		intListNode * temp = cnf->clauses[i].variables->first;
 		while (temp)
 		{
 			printf("%d ", temp->value);
@@ -239,8 +247,8 @@ void DisplayData(CNF * cnf) {
 }
 
 
-bool Process(DecisionTreeNode * node, CNF * cnf) {
-	bool flag = true; //标志是否出现空子句,true为没有出现，false为出现了
+BOOL Process(DecisionTreeNode * node, CNF * cnf) {
+	BOOL flag = TRUE; //标志是否出现空子句,TRUE为没有出现，FALSE为出现了
 
 	int assumption = node->assumption;
 	int currentClauseIndex = 0;
@@ -250,14 +258,14 @@ bool Process(DecisionTreeNode * node, CNF * cnf) {
 	
 
 	if (assumption > 0) {
-		positiveClauseIndexPtr = cnf->variables[assumption].positive.first;
-		negativeClauseIndexPtr = cnf->variables[assumption].negative.first;
-		cnf->variables[assumption].handled = true;					
+		positiveClauseIndexPtr = cnf->variables[assumption].positive->first;
+		negativeClauseIndexPtr = cnf->variables[assumption].negative->first;
+		cnf->variables[assumption].handled = TRUE;					
 	}																
 	else {															
-		negativeClauseIndexPtr = cnf->variables[-assumption].positive.first;
-		positiveClauseIndexPtr = cnf->variables[-assumption].negative.first;
-		cnf->variables[-assumption].handled = true;					
+		negativeClauseIndexPtr = cnf->variables[-assumption].positive->first;
+		positiveClauseIndexPtr = cnf->variables[-assumption].negative->first;
+		cnf->variables[-assumption].handled = TRUE;					
 	}
 
 
@@ -278,12 +286,12 @@ bool Process(DecisionTreeNode * node, CNF * cnf) {
 		currentClauseIndex = negativeClauseIndexPtr->value;
 		// 取负，则将子句中的对应文字删除
 		if (!cnf->clauses[currentClauseIndex].hidden) {
-			assert(cnf->clauses[currentClauseIndex].variables.length > 0);
-			deleteElem(-assumption, &cnf->clauses[currentClauseIndex].variables);
+			assert(cnf->clauses[currentClauseIndex].variables->length >= 0);
+			deleteElem(-assumption, cnf->clauses[currentClauseIndex].variables);
 			--cnf->clauses[currentClauseIndex].length; // 子句长度减少
 			if (0 == cnf->clauses[currentClauseIndex].length) {
 				// 说明出现了空子句
-				flag = false;
+				flag = FALSE;
 			}
 		}
 		negativeClauseIndexPtr = negativeClauseIndexPtr->next;
@@ -297,14 +305,14 @@ void Recover(DecisionTreeNode * node, CNF * cnf) {
 	intListNode * positiveClauseIndexPtr = nullptr;
 	intListNode * negativeClauseIndexPtr = nullptr;
 	if (assumption > 0) {
-		positiveClauseIndexPtr = cnf->variables[assumption].positive.first;
-		negativeClauseIndexPtr = cnf->variables[assumption].negative.first;
-		cnf->variables[assumption].handled = false; // 恢复状态
+		positiveClauseIndexPtr = cnf->variables[assumption].positive->first;
+		negativeClauseIndexPtr = cnf->variables[assumption].negative->first;
+		cnf->variables[assumption].handled = FALSE; // 恢复状态
 	}
 	else {
-		negativeClauseIndexPtr = cnf->variables[-assumption].positive.first;
-		positiveClauseIndexPtr = cnf->variables[-assumption].negative.first;
-		cnf->variables[-assumption].handled = false;
+		negativeClauseIndexPtr = cnf->variables[-assumption].positive->first;
+		positiveClauseIndexPtr = cnf->variables[-assumption].negative->first;
+		cnf->variables[-assumption].handled = FALSE;
 	}
 
 
@@ -315,7 +323,7 @@ void Recover(DecisionTreeNode * node, CNF * cnf) {
 		currentClauseIndex = negativeClauseIndexPtr->value;
 		// 取负，则将子句中的对应文字恢复
 		if (!cnf->clauses[currentClauseIndex].hidden) {
-			recoverElem(-assumption, &cnf->clauses[currentClauseIndex].variables);
+			recoverElem(-assumption, cnf->clauses[currentClauseIndex].variables);
 			++cnf->clauses[currentClauseIndex].length;// 子句长度恢复
 
 		}
@@ -345,10 +353,10 @@ DecisionTreeNode * BackTrack(DecisionTreeNode * node, CNF * cnf) {
 		tempPtr = nullptr;
 	}
 	if (!node->valid) {
-		// 此时是因为valid为false而跳出循环，说明找到了假设点
+		// 此时是因为valid为FALSE而跳出循环，说明找到了假设点
 		Recover(node, cnf);
 		node->assumption = -node->assumption;
-		node->valid = true;
+		node->valid = TRUE;
 		return node;
 	}
 	else {
@@ -362,7 +370,7 @@ void updateCountNum(CNF * cnf) {
 	intListNode * tempNode = nullptr;
 	for (int i = cnf->varNum; i > 0; --i) {
 		count = 0;
-		tempNode = cnf->variables[i].negative.first;
+		tempNode = cnf->variables[i].negative->first;
 		while (tempNode)
 		{
 			if (!cnf->clauses[tempNode->value].hidden)
@@ -371,7 +379,7 @@ void updateCountNum(CNF * cnf) {
 		}
 		cnf->variables[i].negativeCount = count;
 
-		tempNode = cnf->variables[i].positive.first;
+		tempNode = cnf->variables[i].positive->first;
 
 		count = 0;
 		while (tempNode)
@@ -394,7 +402,7 @@ void updateCountNum_2(CNF * cnf) {
 			continue;
 		}
 		else {
-			tempNode = cnf->variables[i].negative.first;
+			tempNode = cnf->variables[i].negative->first;
 			while (tempNode)
 			{
 				if (!cnf->clauses[tempNode->value].hidden && cnf->clauses[tempNode->value].length == 2)
@@ -403,7 +411,7 @@ void updateCountNum_2(CNF * cnf) {
 			}
 			cnf->variables[i].negativeCount = count;
 
-			tempNode = cnf->variables[i].positive.first;
+			tempNode = cnf->variables[i].positive->first;
 
 			count = 0;
 			while (tempNode)
@@ -421,7 +429,8 @@ void updateCountNum_2(CNF * cnf) {
 int FindMax_2(CNF * cnf) {
 	int max = 0;
 	updateCountNum_2(cnf);
-	int maxCount = cnf->variables[max].positiveCount + cnf->variables[max].negativeCount;
+	int maxCount = 0;
+	//int maxCount = cnf->variables[max].positiveCount + cnf->variables[max].negativeCount;
 	for (int i = 1; i <= cnf->varNum; ++i) {
 		if (!cnf->variables[i].handled) {
 			int iCount = cnf->variables[i].positiveCount + cnf->variables[i].negativeCount;
@@ -440,7 +449,8 @@ int FindMax_2(CNF * cnf) {
 int FindMax(CNF *cnf) {
 	int max = 0;
 	updateCountNum(cnf);
-	int maxCount = cnf->variables[max].positiveCount + cnf->variables[max].negativeCount;
+	int maxCount = 0;
+	//int maxCount = cnf->variables[max].positiveCount + cnf->variables[max].negativeCount;
 	for (int i = 1; i <= cnf->varNum; ++i) {
 		if (!cnf->variables[i].handled) {
 			int iCount = cnf->variables[i].positiveCount + cnf->variables[i].negativeCount;
@@ -450,6 +460,7 @@ int FindMax(CNF *cnf) {
 			}
 		}
 	}
+	
 	if (cnf->variables[max].positiveCount > cnf->variables[max].negativeCount)
 		return max;
 	else
@@ -458,20 +469,25 @@ int FindMax(CNF *cnf) {
 
 
 int Findmax_new_2(CNF * cnf) {
-	int max = 0;
-	int max_2 = 0;
+	int max = 0;  // 所有子句中出现最多的元素的序号
+	int max_2 = 0; // 所有长度为2的子句中出现最多的元素的序号
 
 	int max_count_positive = 0;
 	int max_count_negative = 0;
 	int max_count_2_negative = 0;
 	int max_count_2_positive = 0;  
 
-
+	int currentMax_count = 0;
+	int currentMax_count_2 = 0;
 
 	intListNode * tempNode;
 	for (int i = cnf->varNum; i > 0; --i) {
+		//  清零
+		max_count_2_negative = max_count_2_positive = max_count_negative = max_count_positive = 0;
+
 		if (!cnf->variables[i].handled) {
-			tempNode = cnf->variables[i].positive.first;
+			// positive 
+			tempNode = cnf->variables[i].positive->first;
 			while (tempNode) {
 				if (!cnf->clauses[tempNode->value].hidden) {
 					if (cnf->clauses[tempNode->value].length == 2)
@@ -480,29 +496,83 @@ int Findmax_new_2(CNF * cnf) {
 				}
 				tempNode = tempNode->next;
 			}
+			// negative
+			tempNode = cnf->variables[i].negative->first;
+			while (tempNode) {
+				if (!cnf->clauses[tempNode->value].hidden) {
+					if (cnf->clauses[tempNode->value].length == 2)
+						++max_count_2_negative;
+					++max_count_negative;
+				}
+				tempNode = tempNode->next;
+			}
 			
+			// length >= 2
+			if (max_count_negative + max_count_positive > currentMax_count) {
+				max = i;
+				currentMax_count = max_count_negative + max_count_positive;
+			}
+
+			// length == 2
+			if (max_count_2_negative + max_count_2_positive > currentMax_count) {
+				max_2 = i;
+				currentMax_count_2 = max_count_2_negative + max_count_2_positive;
+			}
 		}
 	}
+
+	// 优先选择长度为2的子句中出现次数最多的元素
+	// 其中选择出现次数最多的取值为真
+	if (currentMax_count_2) {
+		if (max_count_2_negative > max_count_2_positive)
+			return -max_2;
+		else
+			return max_2;
+	}
+
+	if (max_count_negative > max_count_positive)
+		return -max;
+	else
+		return max;
+
 	return 0;
 }
 
-
-
-int SelectVar(CNF * cnf) {
-	int max_2 = FindMax_2(cnf);
-	if (max_2)
-		return max_2;
-	else
-		return FindMax(cnf);
+int ReverseOrder(CNF * cnf) {
+	int i = cnf->varNum;
+	while (cnf->variables[i].handled)
+		--i;
+	return i;
 }
 
-bool Satisfied(CNF * cnf) {
+int InOrder(CNF * cnf) {
+	int i = 1;
+	while (cnf->variables[i].handled)
+		++i;
+	if (i > cnf->varNum)
+		return 0;
+	else
+		return i;
+}
+
+int SelectVar(CNF * cnf) {
+	/*int max_2 = FindMax_2(cnf);
+	if (max_2)
+		return -max_2;
+	else
+		return -FindMax(cnf);*/
+	//return InOrder(cnf);
+	//return Findmax_new_2(cnf);
+	return FindMax(cnf);
+}
+
+BOOL Satisfied(CNF * cnf) {
 	// 检查CNF是否已经满足
-	int flag = true;
+	int flag = TRUE;
 	// 如果存在还未被删除的子句则说明没有满足
 	for (int i = cnf->clauseNum; i > 0; --i) {
 		if (!cnf->clauses[i].hidden) {
-			flag = false;
+			flag = FALSE;
 			break;
 		}
 	}
@@ -510,37 +580,28 @@ bool Satisfied(CNF * cnf) {
 }
 
 int SingleRule(CNF * cnf) {
-	// 返回找到的第一个单子句中的变元,0表示不存在单子句
 	int result = 0;
-	for (int i = cnf->clauseNum; i > 0; --i) {
-		if (1 == cnf->clauses[i].length && !cnf->clauses[i].hidden) {
-			result = cnf->clauses[i].variables.first->value;
-			break;
-		}
-	}
-	return result;
-	/*int result = 0;
 	intListNode * tempNode = nullptr;
 	for (int i = cnf->clauseNum; i > 0; --i) {
 		if (1 == cnf->clauses[i].length && !cnf->clauses[i].hidden) {
-			tempNode = cnf->clauses[i].variables.first;
+			tempNode = cnf->clauses[i].variables->first;
 			while (cnf->variables[abs(tempNode->value)].handled)
 				tempNode = tempNode->next;
 			result = tempNode->value;
 			break;
 		}
-	}*/
+	}
 	return result; 
 }
 
 DecisionTreeHead * DPLL(CNF * cnf) {
-	DecisionTreeHead * Head = (DecisionTreeHead *)calloc(1, sizeof(DecisionTreeHead));
+	DecisionTreeHead * Head = (DecisionTreeHead *)malloc(sizeof(DecisionTreeHead));
 
 	assert(Head != nullptr);
 
 	DecisionTreeNode * currentTreeNode = nullptr;
 	Head->firstNode = nullptr;
-	bool flag = true;
+	BOOL flag = TRUE;
 	int singleRuleResult = 0;
 
 	int assumption = 0;
@@ -548,32 +609,31 @@ DecisionTreeHead * DPLL(CNF * cnf) {
 	while (!Satisfied(cnf))
 	{
 		while (flag && (singleRuleResult = SingleRule(cnf))) {
-			currentTreeNode = addAssumption(singleRuleResult, true, Head);
+			currentTreeNode = addAssumption(singleRuleResult, TRUE, Head);
 			flag = Process(currentTreeNode, cnf);
 			
-			printf("proccess the single rule @@ var = %d\n", singleRuleResult);
+			//printf("proccess the single rule @@ var = %d\n", singleRuleResult);
 
 			if (!flag)
 				break;
 		}
-		
 		if (flag) {
 			// 由于不再有单子句才退出
 			assumption = SelectVar(cnf);
 			if (assumption == 0)
 				return Head;
-			currentTreeNode = addAssumption(assumption, false, Head);
-			printf("assumption var = %d\n", assumption);
+			currentTreeNode = addAssumption(assumption, FALSE, Head);
+			//printf("assumption var = %d\n", assumption);
 			flag = Process(currentTreeNode, cnf);
-			printf("process the single rule\n");
+			//printf("process the single rule\n");
 		}
 		else {
 			//  由于存在冲突而退出
-			printf("BAKETRACK @@ var = %d\n", currentTreeNode->assumption);
+			//printf("BAKETRACK @@ var = %d\n", currentTreeNode->assumption);
 			currentTreeNode = BackTrack(currentTreeNode, cnf);
-			printf("AFTER backtrack @@ var = %d\n", currentTreeNode->assumption);
+			//printf("AFTER backtrack @@ var = %d\n", currentTreeNode->assumption);
 			flag = Process(currentTreeNode, cnf);
-			printf("process the single rule\n");
+			//printf("process the single rule\n");
 		}
 		if (nullptr == currentTreeNode)
 			return nullptr;
@@ -592,7 +652,7 @@ void DisplayResult(DecisionTreeHead * Head) {
 
 
 int * TurnToArray(DecisionTreeHead * result, CNF * cnf) {
-	int * resultArray = (int *)calloc((cnf->varNum + 1), sizeof(int));
+	int * resultArray = (int *)malloc((cnf->varNum + 1) * sizeof(int));
 
 	assert(resultArray != nullptr);
 
@@ -613,12 +673,12 @@ void PrintArray(int * resultArray, int length) {
 void CheckFinalResult(int * resultArray, CNF * cnf) {
 	intListNode * tempNode = nullptr;
 	for (int i = 1; i <= cnf->clauseNum; ++i) {
-		bool flag = false;
-		tempNode = cnf->clauses[i].variables.first;
+		BOOL flag = FALSE;
+		tempNode = cnf->clauses[i].variables->first;
 		while (tempNode)
 		{
 			if (resultArray[abs(tempNode->value)] * tempNode->value > 0)
-				flag = true;
+				flag = TRUE;
 			tempNode = tempNode->next;
 		}
 		if (!flag)
@@ -627,22 +687,28 @@ void CheckFinalResult(int * resultArray, CNF * cnf) {
 }
 
 int main(int argc, char ** args) {
-	CNF * cnf = (CNF *)calloc(1, sizeof(CNF));
+	CNF * cnf = (CNF *)malloc(sizeof(CNF));
 
 	assert(cnf != nullptr);
 
 	if (argc == 2)
 		LoadFile(args[1], cnf);
 	else {
-		const char * filename = "problem11-100.cnf";
+		const char * filename = "bart17.shuffled-231.cnf";
 		LoadFile(filename, cnf);
 	}
 
 	//DisplayData(cnf);
+	clock_t startTime = clock();
 	DecisionTreeHead * result = DPLL(cnf);
+	clock_t endTime = clock();
+	int duration = (int)(endTime - startTime);
 	if (result) {
 		//DisplayResult(result);
 		int * resultArray = TurnToArray(result, cnf);  // 最终的结果数组
+
+		printf("DPLL time: %d ms\n", duration);
+
 		PrintArray(resultArray, cnf->varNum+1);
 		CheckFinalResult(resultArray, cnf);
 	}
